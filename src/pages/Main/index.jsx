34389c1863340e8c29e1683e3ab7d23f578ab917
Eugene <skullet-on@mail.ru>
2023-07-18 10:43:52 +0300

    added webrtc application

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import socket from "../../socket";
+import ACTIONS from "../../socket/actions";
+import { v4 } from "uuid";
+
+export default function Main() {
+    const [rooms, updateRooms] = useState([]);
+    const navigate = useNavigate();
+    const rootNode = useRef();
+
+    useEffect(() => {
+        socket.on(ACTIONS.SHARE_ROOMS, ({rooms = []} = {}) => {
+            if (rootNode.current) {    
+                updateRooms(rooms);
+            }
+        })
+    }, [])
+
+    return (
+        <div ref={rootNode}>
+            <h1>Available Rooms</h1>
+            <ul>
+                {rooms.map(roomID => (
+                    <li key={roomID}>
+                        {roomID}
+                        <button onClick={() => {navigate(`/room/${roomID}`)}}>JOIN ROOM</button>
+                    </li>
+                ))}
+            </ul>
+
+            <button onClick={() => {navigate(`/room/${v4()}`)}}> Create new room</button>
+        </div>
+    )
+}
